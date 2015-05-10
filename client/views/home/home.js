dc56b9076ab9d131b6d2ada6af0d80baea3c0daf
Tim Brandin <tim.brandin@gmail.com>
2015-05-10 17:56:01 +0200

    Added helper for channels.

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,3 +1,9 @@
+Template.home.helpers({
+  channels: function() {
+    return Channels.find();
+  }
+});
+
 Template.home.events({
   'submit form': function(event, instance) {
     // We are building an application, so we don't want the form to reload the page.
