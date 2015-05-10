60c5f4dcfafcf8a6af8a223a4221c9d7e09edc9f
Tim Brandin <tim.brandin@gmail.com>
2015-05-10 20:51:59 +0200

    Added form for adding messages.

--- client/views/channel/channel.js
+++ client/views/channel/channel.js
@@ -4,3 +4,14 @@ Template.channel.helpers({
     return Channels.findOne({_id: _id});
   }
 });
+
+Template.channel.events({
+  'keyup form': function(event, instance) {
+    if (event.keyCode == 13 && !event.shift) { // Check if enter was pressed (but without shift).
+      var _id = Router.current().params._id;
+      var value = instance.find('textarea').value;
+      instance.find('textarea').value = ''; // Clear the textarea.
+      Messages.insert({_channel: _id, message: value});
+    }
+  }
+});
