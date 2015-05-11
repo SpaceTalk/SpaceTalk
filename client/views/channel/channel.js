963c48e7a5cb7f552087c32df77d084bbc8f6108
Tim Brandin <tim.brandin@gmail.com>
2015-05-11 15:38:13 +0200

    Added styling.

--- client/views/channel/channel.js
+++ client/views/channel/channel.js
@@ -3,8 +3,15 @@ Template.channel.onCreated(function() {
   // Listen for changes to reactive variables (such as Router.current()).
   instance.autorun(function() {
     var channel = Router.current().params._id;
-    instance.subscribe('messages', channel);
+    var sub = instance.subscribe('messages', channel);
+    if (sub.ready()) {
+      window.scrollTo(0, document.body.scrollHeight);
+    }
   });
+});
+
+Template.channel.onRendered(function() {
+  $('article').css({'padding-bottom': $('footer').outerHeight()});
 });
 
 Template.channel.helpers({
@@ -32,12 +39,20 @@ Template.channel.helpers({
     if (!instance.date || instance.date != dateNow) {
       return instance.date = dateNow;
     }
+  },
+
+  avatar: function() {
+    var user = Meteor.users.findOne({_id: this._userId});
+    if (user && user.emails) {
+      return Gravatar.imageUrl(user.emails[0].address);
+    }
   }
 });
 
 Template.messageForm.events({
-  'keyup textarea': function(event, instance) {
+  'keydown textarea': function(event, instance) {
     if (event.keyCode == 13 && !event.shiftKey) { // Check if enter was pressed (but without shift).
+      event.preventDefault();
       var _id = Router.current().params._id;
       var value = instance.find('textarea').value;
       // Markdown requires double spaces at the end of the line to force line-breaks.
@@ -49,6 +64,10 @@ Template.messageForm.events({
         _userId: Meteor.userId(), // Add userId to each message.
         timestamp: new Date() // Add a timestamp to each message.
       });
+      // Restore the autosize value.
+      instance.$('textarea').css({height: 37});
+      window.scrollTo(0, document.body.scrollHeight);
     }
+    $('article').css({'padding-bottom': $('footer').outerHeight()});
   }
 });
--- client/views/home/home.js
+++ client/views/home/home.js
@@ -6,10 +6,26 @@ Template.home.onCreated(function() {
 Template.home.helpers({
   channels: function() {
     return Channels.find();
+  },
+
+  avatar: function() {
+    var user = Meteor.user();
+    if (user && user.emails) {
+      return Gravatar.imageUrl(user.emails[0].address);
+    }
+  },
+
+  active: function() {
+    var _id = Router.current().params._id;
+    return _id == this._id ? 'active' : '';
   }
 });
 
-Template.home.events({
+Template.channelForm.onCreated(function() {
+  $('textarea').autosize();
+});
+
+Template.channelForm.events({
   'submit form': function(event, instance) {
     // We are building an application, so we don't want the form to reload the page.
     event.preventDefault();
@@ -18,5 +34,17 @@ Template.home.events({
     instance.find('input').value = '';
 
     Channels.insert({name: name});
+
+    // Hide form when submitted.
+    instance.$('.add-channel-form').addClass('hidden');
+  },
+
+  'click .show-form': function(event, instance) {
+    // We are building an application, so we don't want the form to reload the page.
+    event.preventDefault();
+
+    // Show form.
+    instance.$('.add-channel-form').toggleClass('hidden');
+    instance.$('.add-channel-form input').focus();
   }
 });
