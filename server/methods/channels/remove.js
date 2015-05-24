2587d6b87e75b8268ad0160962e77fa0e0959ab2
Richard Lai <it.rclai@gmail.com>
2015-05-24 00:23:42 -0400

    Secure checks before removing channels

--- server/methods/channels/remove.js
+++ server/methods/channels/remove.js
@@ -1,8 +1,18 @@
 Meteor.methods({
-  'channels.remove': function(channelId) {
+  'channels.remove': function (channelId) {
     check(channelId, String);
+
+    // XXX TODO - Need to check if the user can remove the channel
+    if (!this.userId) {
+      throw new Meteor.Error(401, 'Unauthorized access');
+    }
+
+    // Check channel exists
+    if (!Channels.findOne(channelId)) {
+      throw new Meteor.Error(404, 'Channel does not exist');
+    }
     
-    Messages.remove({channelId: channelId});
-    Channels.remove({_id: channelId});
+    Messages.remove({ channelId: channelId });
+    Channels.remove({ _id: channelId });
   }
 });
