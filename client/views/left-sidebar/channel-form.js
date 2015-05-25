e9669f4f5611381410f7a338fdcb74b7abb4ca1f
Adam Dawkins <adamdawkins@gmail.com>
2015-05-25 16:55:48 +0100

    Add spaces between function and (

--- client/views/left-sidebar/channel-form.js
+++ client/views/left-sidebar/channel-form.js
@@ -15,7 +15,7 @@ ChannelForm = BlazeComponent.extendComponent({
         if ($.trim(name) === "") return;
 
         // Allow only unique channel name
-        Meteor.call('channels.add', currentTeamId(), name, function(error, result) {
+        Meteor.call('channels.add', currentTeamId(), name, function (error, result) {
           if (result) {
             // Navigate to the new channel view
             var newChannel = Channels.findOne(result);
@@ -54,7 +54,7 @@ ChannelForm = BlazeComponent.extendComponent({
                   closeOnCancel: true,
                   cancelButtonText: "OK",
                   html: true
-                }, function(isConfirm) {
+                }, function (isConfirm) {
                   // User wants to visit the existing channel,
                   // clear the input and hide the form
                   // and redirect to the requested channel
