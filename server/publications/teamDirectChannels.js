231474c4af92b5164f520d5ffdad3f468792af10
Josh Owens <joshua.owens@gmail.com>
2015-05-25 09:37:26 -0400

    Restrict our direct channel pub to only the logged in users channels.

--- server/publications/teamDirectChannels.js
+++ server/publications/teamDirectChannels.js
@@ -3,7 +3,7 @@ Meteor.publish('teamDirectChannels', function(search) {
   if (this.userId) {
     var team = Teams.findOne({ $or: [{ _id: search }, { slug: search }] });
     const teamId = team ? team._id : null;
-    return Channels.find({ teamId: teamId, direct: true});
+    return Channels.find({ teamId: teamId, direct: true, allowedUsers: {$in: [this.userId]}});
   }
   this.ready();
 });
