db218f17440a9a381a709c93995d4f9f365deefb
Dmitrijs Balcers <dmitrijs.balcers@gmail.com>
2015-05-26 12:14:44 +0300

    #64 extract channel updateTopic method in separate file. Fix comment

--- server/methods/channels/add.js
+++ server/methods/channels/add.js
@@ -37,24 +37,5 @@ Meteor.methods({
     } else {
       throw new Meteor.Error(422, 'Channel name exists');
     }
-  },
-
-  'channels.updateTopic': function (id, topic) {
-    check(id, String);
-    check(topic, String);
-
-    // Check user authenticated
-    if (!this.userId) {
-      throw new Meteor.Error(401, 'Unauthorized access');
-    }
-
-    // Check team exist
-    if (!Channels.findOne({ _id: id })) {
-      throw new Meteor.Error(404, 'Channel does not exist');
-    }
-
-    Channels.update(id, {
-      $set: {topic: topic}
-    });
   }
 });
