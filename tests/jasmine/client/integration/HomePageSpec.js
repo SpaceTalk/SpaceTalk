459043afcc7f498d0ba1beb1cba016bf86eac106
Dennis Stolmeijer <me@dennisstolmeijer.nl>
2015-05-27 23:12:58 +0200

    added simple first test #84

---
+++ tests/jasmine/client/integration/HomePageSpec.js
@@ -0,0 +1,9 @@
+/**
+ * Tests for the homepage functionality
+ */
+
+describe("Logging in on the homepage", function () {
+  it("should login and stay on the homepage", function () {
+    expect(true).toBe(true);
+  });
+});
