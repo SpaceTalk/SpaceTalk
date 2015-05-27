3ab2219374e09ea8b31df23ef8e6a0b98e08a165
Richard Lai <it.rclai@gmail.com>
2015-05-26 22:28:25 -0400

    No more scroll position calculation

--- client/lib/dom-utilities.js
+++ client/lib/dom-utilities.js
@@ -1,16 +1,11 @@
 /**
  * Scrolls down the page when the user is a at or nearly at the bottom of the page
  */
-scrollDown = function (force) {
+scrollDown = function () {
   // It has to be in a setTimeout or it won't
   // scroll all the way down for some reason
   setTimeout(function () {
-    // Check if the innerHeight + the scrollY position is higher than the offsetHeight - 200
-    if ((window.innerHeight + window.scrollY) >= (
-      Number(document.body.offsetHeight) - 200
-      ) || force) {
-      // Scroll down the page
-      window.scrollTo(0, document.body.scrollHeight);
-    }
+    // Scroll down the page
+    window.scrollTo(0, document.body.scrollHeight);
   }, 0);
 };
