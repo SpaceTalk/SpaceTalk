00d96f2b938832a808ed92ef32e0150d84020194
Jon Earley <jonathan.a.earley@gmail.com>
2015-05-28 14:50:15 -0500

    Emoji autocomplete

--- client/views/channel/channel.js
+++ client/views/channel/channel.js
@@ -58,7 +58,7 @@ Channel = BlazeComponent.extendComponent({
   events: function () {
     return [
       {
-        'keydown textarea[name=message]': function (event) {
+        'keyup textarea[name=message]': function (event) {
           if (event.keyCode == 13 && !event.shiftKey) { // Check if enter was pressed (but without shift).
             event.preventDefault();
             var _id = currentRouteId();
@@ -81,6 +81,49 @@ Channel = BlazeComponent.extendComponent({
             });
             window.scrollTo(0, document.body.scrollHeight);
           }
+
+          $("textarea").textcomplete([ {
+            match: /\B:([\-+\w]*)$/,
+            search: function (term, callback) {
+              var results = [];
+              var results2 = [];
+              var results3 = [];
+              $.each(emojiStrategy,function(shortname,data) {
+                if(shortname.indexOf(term) > -1) { results.push(shortname); }
+                else {
+                  if((data.aliases !== null) && (data.aliases.indexOf(term) > -1)) {
+                    results2.push(shortname);
+                  }
+                  else if((data.keywords !== null) && (data.keywords.indexOf(term) > -1)) {
+                    results3.push(shortname);
+                  }
+                }
+              });
+
+              if(term.length >= 3) {
+                results.sort(function(a,b) { return (a.length > b.length); });
+                results2.sort(function(a,b) { return (a.length > b.length); });
+                results3.sort();
+              }
+              var newResults = results.concat(results2).concat(results3);
+
+              callback(newResults);
+            },
+            template: function (shortname) {
+              return '<img class="emojione" src="//cdn.jsdelivr.net/emojione/assets/png/'+emojiStrategy[shortname].unicode+'.png"> :'+shortname+':';
+            },
+            replace: function (shortname) {
+              return ':'+shortname+': ';
+            },
+            index: 1,
+            maxCount: 10,
+          }
+          ]);
+
+          $('.dropdown-menu').prependTo('.message-tab-content');
+          $('.dropdown-menu').css({
+            "position": "static",
+          });
         },
         'click [data-action="remove-channel"]': function (event) {
           event.preventDefault();
