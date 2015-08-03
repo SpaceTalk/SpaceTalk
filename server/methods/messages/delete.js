730484320dc0c464f43a06deb95c18996f6322de
Yanis Ikene <yanisIk@users.noreply.github.com>
2015-08-02 20:43:35 -0400

    Validation order and performance optimization in delete.js
    - Validation order :
    Check if the message exists in the DB after checking if the user is authenticated.
    
    - Performance optimization :
    Use cursor.count() instead of findOne for better performance since we do not need the Message entity after that.

--- server/methods/messages/delete.js
+++ server/methods/messages/delete.js
@@ -1,14 +1,13 @@
 Meteor.methods({
 	'messages.delete': function (messageId) {
 		check(messageId, String);
-    var message = Messages.findOne(messageId);
 
 		if (!this.userId) {
-	      	throw new Meteor.Error(401, 'Unauthorized access');
+	    throw new Meteor.Error(401, 'Unauthorized access');
 		}
 
-		if (!message) {
-			throw new Meteor.Error(404, 'Message does not exist');
+		if (!Messages.find(messageId).count()) {
+		  throw new Meteor.Error(404, 'Message does not exist');
 		}
 
 		if(isOwner('Messages', messageId)) {
